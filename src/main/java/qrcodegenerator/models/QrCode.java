d893dfffd3998919b151bdde68381e75218b7d3d
jueverhard <jueverhard@hotmail.fr>
2025-10-17 02:09:08 +0200

    ✨ Basic QR Code visualization implementation

--- src/main/java/qrcodegenerator/models/QrCode.java
+++ src/main/java/qrcodegenerator/models/QrCode.java
@@ -1,5 +1,7 @@
 package qrcodegenerator.models;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Logger;
 
 public class QrCode {
@@ -8,8 +10,12 @@ public class QrCode {
 
     private final Version version;
 
+    private final Set<Position> blacks;
+
     public QrCode(String chainToRepresent) {
         this.version = Version.fromChainSize(chainToRepresent.length());
+        // TODO JEV : defines every black positions according to the QR Code encoding specs
+        this.blacks = new HashSet<>();
     }
 
     /**
@@ -17,5 +23,18 @@ public class QrCode {
      */
     public void print() {
         log.info("Required version identified: " + this.version);
+        StringBuilder builder = new StringBuilder("Here's the corresponding QR Code:\n");
+        for (int y = 0; y < this.version.getCodeWidth(); y++) {
+            for (int x = 0; x <= this.version.getCodeWidth(); x++) {
+                int finalX = x;
+                int finalY = y;
+                boolean isBlack = this.blacks.stream()
+                        .anyMatch(black -> finalX == black.x() && finalY == black.y());
+                builder.append(isBlack ? '#' : '.');
+            }
+            builder.append("\n");
+        }
+
+        log.info(builder.toString());
     }
 }
