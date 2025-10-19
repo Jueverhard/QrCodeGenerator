014b094a6ebd12bff21870f542ac4fec7fe3925f
jueverhard <jueverhard@hotmail.fr>
2025-10-19 22:26:27 +0200

    ✨ Generates every format pixels

--- src/main/java/qrcodegenerator/models/QrCode.java
+++ src/main/java/qrcodegenerator/models/QrCode.java
@@ -1,6 +1,5 @@
 package qrcodegenerator.models;
 
-import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -12,12 +11,12 @@ public class QrCode {
 
     private final Version version;
 
-    private final Set<Position> blacks;
+    private final Set<Position> pixels;
 
     public QrCode(String chainToRepresent) {
         this.version = Version.fromChainSize(chainToRepresent.length());
         // TODO JEV : defines every black positions according to the QR Code encoding specs
-        this.blacks = this.generateCorners();
+        this.pixels = this.generateFormatPixels();
     }
 
     /**
@@ -26,11 +25,11 @@ public class QrCode {
     public void print() {
         log.info("Required version identified: " + this.version);
         StringBuilder builder = new StringBuilder("Here's the corresponding QR Code:\n");
-        for (int y = 0; y <= this.version.getCodeWidth(); y++) {
-            for (int x = 0; x <= this.version.getCodeWidth(); x++) {
+        for (int y = 0; y < this.version.getCodeWidth(); y++) {
+            for (int x = 0; x < this.version.getCodeWidth(); x++) {
                 int finalX = x;
                 int finalY = y;
-                boolean isBlack = this.blacks.stream()
+                boolean isBlack = this.pixels.stream()
                         .anyMatch(black -> finalX == black.x() && finalY == black.y());
                 builder.append(isBlack ? '#' : '.');
             }
@@ -41,32 +40,64 @@ public class QrCode {
     }
 
     /**
-     * Generates the top-left, top-right and bottom-left positioning corners.
-     * @return Every corner-filled positions.
+     * Generates every format pixels: this includes the top-left, top-right and bottom-left corners, the smaller
+     * bottom-right square, and the corners joining dots.
+     *
+     * @return Every format pixels.
      */
-    private Set<Position> generateCorners() {
-        // Generates full corners
-        int upperBound = this.version.getCodeWidth();
-        Set<Position> corners = IntStream.range(0, 7)
-                .mapToObj(x -> IntStream.range(0, 7)
-                        .mapToObj(y -> Set.of(
-                                new Position(x, y),
-                                new Position(x, upperBound - y),
-                                new Position(upperBound - x, y)
-                        )).flatMap(Set::stream)
-                        .toList()
+    private Set<Position> generateFormatPixels() {
+        // Generates fully-filled corners
+        int upperBound = this.version.getCodeWidth() - 1;
+        Set<Position> fixedPixels = generatePositioningSquare(new Position(0, 0), 7);
+        fixedPixels.addAll(generatePositioningSquare(new Position(0, upperBound - 6), 7));
+        fixedPixels.addAll(generatePositioningSquare(new Position(upperBound - 6, 0), 7));
+
+        // Generates smaller bottom-right square
+        fixedPixels.addAll(generatePositioningSquare(
+                new Position(upperBound - 8, upperBound - 8),
+                5
+        ));
+
+        // Generates joining dots
+        IntStream.range(8, upperBound - 6)
+                .filter(i -> i % 2 == 0)
+                .mapToObj(i -> Set.of(
+                        new Position(6, i),
+                        new Position(i, 6)
+                ))
+                .flatMap(Set::stream)
+                .forEach(fixedPixels::add);
+
+        // Generates fixed bottom-left dark pixel
+        fixedPixels.add(new Position(8, upperBound - 7));
+
+        return fixedPixels;
+    }
+
+    /**
+     * Generates a positioning square of given size at given position.
+     *
+     * @param topLeft Top-left position of the square.
+     * @param size    Size of the square.
+     * @return The set of pixels composing the square.
+     */
+    private Set<Position> generatePositioningSquare(Position topLeft, int size) {
+        // Generates a fully-filled square of given size at given position
+        Set<Position> squarePixels = IntStream.range(topLeft.x(), topLeft.x() + size)
+                .mapToObj(x -> IntStream.range(topLeft.y(), topLeft.y() + size)
+                        .mapToObj(y -> new Position(x, y))
                 )
-                .flatMap(List::stream)
+                .flatMap(stream -> stream)
                 .collect(Collectors.toSet());
 
-        // Removes empty fields
-        corners.removeIf(pos -> Set.of(1, 5, upperBound - 5, upperBound - 1).contains(pos.x()) &&
-                !Set.of(0, 6, upperBound - 6, upperBound).contains(pos.y())
+        // Removes inner empty part
+        squarePixels.removeIf(pos -> Set.of(topLeft.x() + 1, topLeft.x() + size - 2).contains(pos.x()) &&
+                !Set.of(topLeft.y(), topLeft.y() + size - 1).contains(pos.y())
         );
-        corners.removeIf(pos -> Set.of(1, 5, upperBound - 5, upperBound - 1).contains(pos.y()) &&
-                !Set.of(0, 6, upperBound - 6, upperBound).contains(pos.x())
+        squarePixels.removeIf(pos -> Set.of(topLeft.y() + 1, topLeft.y() + size - 2).contains(pos.y()) &&
+                !Set.of(topLeft.x(), topLeft.x() + size - 1).contains(pos.x())
         );
 
-        return corners;
+        return squarePixels;
     }
 }
